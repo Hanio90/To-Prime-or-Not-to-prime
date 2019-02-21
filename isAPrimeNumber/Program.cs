e7252a24e64bbaf30007d46246b727b2d2b2b55b
Hani Osman <hanio90@gmail.com>
2019-02-20 23:01:10 -0500

    To prime or Not to Prime.

---
+++ isAPrimeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace isAPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = Numbers.isAPrimeNumber(150) ? "Is a prime number!" : "Is not a prime number!";
+            Console.WriteLine(message);
+
+            message = Numbers.isAPrimeNumber(2) ? "Is a prime number!" : "Is not a prime number!";
+            Console.WriteLine(message);
+
+            message = Numbers.isAPrimeNumber(53) ? "Is a prime number!" : "Is not a prime number!";
+            Console.WriteLine(message);
+
+            message = Numbers.isAPrimeNumber(23123) ? "Is a prime number!" : "Is not a prime number!";
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+
+            /*
+            OUTPUT:
+            
+            Is not a prime number!
+            Is a prime number!
+            Is a prime number!
+            Is not a prime number!
+
+            */
+        }
+    }
+}
---
+++ isAPrimeNumber/class/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace isAPrimeNumber
+{
+    public class Numbers
+    {
+        public static bool isAPrimeNumber(int num)
+        {
+            bool returnMsg = false;
+
+            for (int i = 2; i <= num; i++)
+            {
+                if ((num % i) == 0 && num != i)
+
+                {
+                    returnMsg = false;
+                    return returnMsg;
+                }
+                else if (num == i)
+                {
+                    returnMsg = true;
+                    return returnMsg;
+                }
+            }
+            return returnMsg;
+        }
+    }
+}
